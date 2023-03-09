27f7fb3238e67d8a81264412a66cc5a23c34b69e
Your Name <you@example.com>
2023-03-09 19:38:26 +0300

    task3

--- Lesson1/Homework/Task3.java
+++ Lesson1/Homework/Task3.java
@@ -1,5 +1,29 @@
-package Lesson1.Homework;
-
+import java.util.Scanner;
 public class Task3 {
-    
+   public static void main(String[] args) {
+      double num1;
+      double num2;
+      double ans;
+      char op;
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Введите 2 числа через пробел: ");
+      num1 = reader.nextDouble();
+      num2 = reader.nextDouble();
+      System.out.print("\nВведите оператор (+, -, *, /): ");
+      op = reader.next().charAt(0);
+      switch(op) {
+         case '+': ans = num1 + num2;
+            break;
+         case '-': ans = num1 - num2;
+            break;
+         case '*': ans = num1 * num2;
+            break;
+         case '/': ans = num1 / num2;
+            break;
+         default:  System.out.printf("Error!");
+            return;
+      }
+      System.out.print("\nРезультат:\n");
+      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+   }
 }
